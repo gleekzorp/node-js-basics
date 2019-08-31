cd52a1005c3046ca048d3397d38511bbf1d44082
gleekzorp <gleek.dan@gmail.com>
2019-08-31 14:55:34 -0600

    Update and Delete functionality working

--- 09-mongodb/routes/index.js
+++ 09-mongodb/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 
 var url = 'mongodb://localhost:27017/test';
@@ -10,10 +11,10 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-// Get Data
+// Read Data
 router.get('/get-data', function(req, res, next) {
   var resultArray = []
-  
+
   mongo.connect(url, function(err, client) {
     assert.equal(null, err);
     var db = client.db('test');
@@ -28,14 +29,14 @@ router.get('/get-data', function(req, res, next) {
   });
 });
 
-// Post Data
+// Create Data
 router.post('/insert', function(req, res, next) {
   var item = {
     title: req.body.title,
     content: req.body.content,
     author: req.body.author
   };
-
+  
   mongo.connect(url, function(err, client) {
     assert.equal(null, err);
     var db = client.db('test');
@@ -48,12 +49,39 @@ router.post('/insert', function(req, res, next) {
   res.redirect('/');
 });
 
+// Update Data
 router.post('/update', function(req, res, next) {
-
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+  var id = req.body.id;
+  
+  mongo.connect(url, function(err, client) {
+    assert.equal(null, err);
+    var db = client.db('test');
+    db.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item updated');
+      client.close();
+    });
+  });
 })
 
+// Delete Data
 router.post('/delete', function(req, res, next) {
-
+  var id = req.body.id;
+  
+  mongo.connect(url, function(err, client) {
+    assert.equal(null, err);
+    var db = client.db('test');
+    db.collection('user-data').deleteOne({"_id": objectId(id)}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item deleted');
+      client.close();
+    });
+  });
 })
 
 module.exports = router;
